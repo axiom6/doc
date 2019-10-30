3b7673bd421a175fb6f027346775e9e6f2afe818
Tom Flaherty <tef@eazy.net>
2019-10-30 13:38:19 -0600

    Wed Oct 30   /lib/js and /lib/js   Began converting slide to /aug/talk

--- talk/lib/js/RevealInit.js
+++ talk/lib/js/RevealInit.js
@@ -3,31 +3,32 @@
 // Full list of configuration options available here:
 // https://github.com/hakimel/reveal.js#configuration
 $(document).ready( function() {
-  var modules = '../../node_modules/';
-  var revdir  = modules + 'reveal.js/';
-  initialize( revdir );
-  pdfCSS(     revdir ) } );
+  var jsdir  = '../lib/';
+  var cssdir = '../../lib/';
+  var hljs    = window['hljs'];
+  initialize( jsdir, hljs );
+  pdfCSS(     cssdir ) } );
 
 
-initialize = function(  revdir ) {
+initialize = function(  revdir, hljs ) {
   Reveal.initialize( { controls:true, progress:true, history:true, center:true, slideNumber:true,
     transition: 'slide', // none/fade/slide/convex/concave/zoom
     dependencies: [                                            // Optional libraries used to extend on reveal.js
-      { src: revdir + 'lib/js/classList.js',           condition: function() { return !document.body.classList; } },
-      { src: revdir + 'plugin/markdown/marked.js',     condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-      { src: revdir + 'plugin/markdown/markdown.js',   condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-      { src: revdir + 'plugin/highlight/highlight.js', condition: function() { return !!document.querySelector( 'pre code'        ); },
-                                                                               callback:  function() { hljs.initHighlightingOnLoad(); }, async:true },
-      { src: revdir + 'plugin/zoom-js/zoom.js', async: true },
-      { src: '../../lib/js/notes.js',  async: true }    ]
+    //{ src: revdir + 'js/zoom.js',      async: true },
+    //{ src: revdir + 'js/notes.js',     async: true },
+      { src: revdir + 'js/classList.js', condition: function() { return !document.body.classList; } },
+      { src: revdir + 'js/marked.js',    condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+      { src: revdir + 'js/markdown.js',  condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+      { src: revdir + 'js/highlight.js', condition: function() { return !!document.querySelector( 'pre code'        ); },
+        callback:  function() { hljs.initHighlightingOnLoad(); }, async:true } ]
   }); };
 
 // PDF and Paper printing
 pdfCSS = function(  revdir ) {
   var link = document.createElement( 'link' );
-  link.rel = 'stylesheet';
+  link.rel  = 'stylesheet';
   link.type = 'text/css';
   link.href = window.location.search.match( /print-pdf/gi )
-    ? revdir + 'css/print/pdf.css'
-    : revdir + 'css/print/paper.css';
+    ? revdir + 'css/pdf.css'
+    : revdir + 'css/paper.css';
   document.getElementsByTagName( 'head' )[0].appendChild( link ); };
